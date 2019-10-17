3a0aa391882763c786aec970c8722ff11c3c0f0b
Michael Calkins <m.calkins576@gmail.com>
2019-10-17 17:36:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,10 @@ def square_array(array)
   counter = 0
   arr = Array.new(array.length)
   while array[counter] do
-    arr = [array[counter]**2]
+    arr[counter] = array[counter]**2
     counter += 1
-  end
   array = arr
+  end
   p arr
   p array
 end
